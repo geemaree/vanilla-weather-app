34530df3e21b3994ff726c88c9df2455cb3e55b7
geemaree <115192851+geemaree@users.noreply.github.com>
2022-11-26 14:42:52 +1300

    remove console.log

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
